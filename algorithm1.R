9da33bd993928f14aa6d501aea84ef7086d8d62d
Sibei Liu <Sibei@SibeideMacBook-Pro.local>
2024-03-19 15:00:45 -0400

    add the sigma

--- algorithm1.R
+++ algorithm1.R
@@ -6,7 +6,7 @@ p2=5
 tol=exp(-10)
 dat_sim=function(n,p,p2,beta,sig){
   X=matrix(rnorm(n=n*p, mean =0, sd = 1),nrow=n) # small sd of X
-  e=rnorm(n,sd = sig)
+  e=rnorm(n,sd = sig)                            # e with mean=0 and sd=1
   Y= X %*% beta+ X%*% gamma *e  
   return(list(X=X, Y=Y))
 }
@@ -20,7 +20,8 @@ df=data.frame(X,Y=Y)
 w_est <- function(gamma){
   W <- NULL
   for (i in 1:n){
-    new <- 1/((t(X[i,]^2)%*% gamma))
+    #new <- 1/((t(X[i,]^2)%*% gamma)) 
+    new <- 1/((t(X[i,]^2)%*% gamma^2))
     W <- c(W,new)
   }
   W <- diag(W)
@@ -36,14 +37,16 @@ beta_est=function(gamma){
 
 gamma_est=function(beta){
   X2 <- X^2
-  gamma <- (solve(t(X2)%*% X2) %*% t(X2) %*% (Y-X%*% beta )^2)
+  gamma <- (solve(t(X2)%*% X2) %*% t(X2) %*% (Y-X%*% beta )^2) /sig # Sibei: I add a /sigma
   return(gamma)
 }
 ##### check if the above result is correct
-beta_est(rep(0.3^2,p))
-gamma_est(rep(1,p))
+
+beta_est(rep(0.3,p)) # input the real value of gamma, gives us the right real beta
+gamma_est(rep(1,p)) # input the real value of beta, NOT !!! gives us the right real gamma
+
 #gamma0=rep(0.2,p) # initial
-beta=rep(0.5,p)
+beta=rep(0.5,p)    # initial guess
 sum(abs(coef(lm(Y~.-1,data=df))-rep(1,5))) # the results from OLS
 diff1 <- diff2 <- 10
 # gamma=gamma_est(beta,sig=sig) #rep(0.3,5)#
@@ -62,6 +65,6 @@ while ( diff1 > tol & M < 100) { #diff2 >tol &
 
 beta
 sum(abs(beta-rep(1,5)))
-sqrt(abs(gamma)) ## since the estimated gamma is actually gamma^2
+abs(gamma) 
 
 
