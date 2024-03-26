10ed3fa68dd58ee61f3b6c4ef5793d4fb44d9984
Sibei Liu <Sibei@SibeideMacBook-Pro.local>
2024-03-26 12:27:45 -0400

    Mar 25 updates

--- simulation.R
+++ simulation.R
@@ -14,7 +14,8 @@ dat_sim=function(n,beta_real,sig,gamma_real){
   X=matrix(rnorm(n=n*p, mean =0, sd = 1),nrow=n) # small sd of X
   e=rnorm(n,sd = sig)                            # e with mean=0 and sd=1
   Y= X %*% beta_real+ X %*% gamma_real *e  
-  return(list(X=X, Y=Y))
+  Y2=X %*% beta_real+e
+  return(list(X=X, Y=Y,Y2=Y2))
 }
 
 
@@ -66,36 +67,45 @@ return(beta)
 }
 
 ####
-sim_varguild=function(beta_real,gamma_real,sig=sig,nsim){
-  MSE1=MSE2=NULL
+sim_varguid=function(beta_real,gamma_real,sig=sig,nsim){
+  MSE1=MSE2=MSE3=NULL
 for (i in 1:nsim) {
   sim=dat_sim(n,beta_real,sig = sig,gamma_real) 
-  m1=lm(Y~.-1,data=data.frame(X=sim[[1]],Y=sim[[2]])) 
+  m1=lm(Y~.-1,data=data.frame(X=sim[[1]],Y=sim[[3]])) 
   MSE1[i]=mean((coef(m1)-beta_real)^2)
   
+  m2=lm(Y~.-1,data=data.frame(X=sim[[1]],Y=sim[[2]])) 
+  MSE2[i]=mean((coef(m2)-beta_real)^2)
+  
   beta2=var_est(beta_real,X=sim[[1]],Y=sim[[2]])
-  MSE2[i]=mean((beta2-beta_real)^2)
+  MSE3[i]=mean((beta2-beta_real)^2)
 }
-  return(list(MSE1=MSE1,MSE2=MSE2))
+  return(list(MSE1=MSE1,MSE2=MSE2,MSE3=MSE3))
 }
 
 
 #########
 p=5;p2=5
-res5=sim_varguild(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
-colMeans(as.data.frame(res5))
+res5=sim_varguid(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
 p=10;p2=10
-res10=sim_varguild(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
+res10=sim_varguid(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
 p=20;p2=20
-res20=sim_varguild(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
+res20=sim_varguid(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
 p=50;p2=50
-res50=sim_varguild(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
-colMeans(as.data.frame(res50))
+res50=sim_varguid(beta_real=rep(1,p),gamma_real=rep(1,p),sig=1,nsim)
+
+
 par(mfrow = c(2, 2))
-boxplot(res5[[1]],res5[[2]],names=c("OLS","varGuild"),ylab='MSE',main="p=5")
-boxplot(res10[[1]],res10[[2]],names=c("OLS","varGuild"),ylab='MSE',main="p=10")
-boxplot(res20[[1]],res20[[2]],names=c("OLS","varGuild"),ylab='MSE',main="p=20")
-boxplot(res50[[1]],res50[[2]],names=c("OLS","varGuild"),ylab='MSE',main="p=50")
+boxplot(res5[[1]],res5[[2]],res5[[3]],names=c("OLS in normal setting","OLS in gamma setting","varGuid"),ylab='MSE',main="p=5")
+boxplot(res10[[1]],res10[[2]],res10[[3]],names=c("OLS in normal setting","OLS in gamma setting","varGuid"),ylab='MSE',main="p=10")
+boxplot(res20[[1]],res20[[2]],res20[[3]],names=c("OLS in normal setting","OLS in gamma setting","varGuid"),ylab='MSE',main="p=20")
+boxplot(res20[[1]],res50[[2]],res50[[3]],names=c("OLS in normal setting","OLS in gamma setting","varGuid"),ylab='MSE',main="p=50")
+
+
+MSE_table=rbind(colMeans(as.data.frame(res5)),colMeans(as.data.frame(res10)),
+                colMeans(as.data.frame(res20)),colMeans(as.data.frame(res50)))
+
+rownames(MSE_table)=c("p=5","p=10","p=20","p=50")
 
 
 ## Task
