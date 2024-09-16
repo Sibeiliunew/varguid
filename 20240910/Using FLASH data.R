423a7fe0f780ed20eafe0988d730ccb06078c7d2
Sibei Liu <Sibei@Sibeis-work-buddy.local>
2024-09-16 10:33:33 -0400

    flash file organiza

---
+++ 20240910/Using FLASH data.R
@@ -0,0 +1,17 @@
+source("./leash2.0.6.R")
+source("./VarGuid20240626.R")
+library(glmnet)
+library(tidyverse)
+library(MASS)
+library(xtable)
+library(scales)
+library(faux)
+library(olsrr)
+library(caTools)
+
+FLASH=readRDS("flash.data.1003.rds")
+
+## the boxplot
+
+
+### the correlation
